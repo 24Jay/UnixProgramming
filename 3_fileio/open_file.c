1cc0f8c963cfc26cdd66095275d0deca78d92c2a
Jay <24zhangjie@gmail.com>
2017-04-24 21:52:48 +0800

    read a specified file and print to standout

--- 3_fileio/open_file.c
+++ 3_fileio/open_file.c
@@ -2,14 +2,20 @@
 #include <fcntl.h>
 #define BUFFSIZE 1024
 
-
+/*
+ * read a specified file , and print to stdout
+ *
+ */
 int main(void)
 {
 	char buf[BUFFSIZE];
 	int n;
-	int fd = open("test.txt",O_WRONLY,FILE_MODE);
-	while((n=read(fd,buf,BUFFSIZE)>0))
+	int fd = open("test.txt",O_RDWR,FILE_MODE);
+//	printf("fd=%d\n",fd);
+	while((n=read(fd,buf,BUFFSIZE))>0)
 	{
+//		printf("n=%d\n",n);
+//		printf("buf = %s\n",buf);
 		if(write(STDOUT_FILENO,buf,n)!=n)
 			err_sys("write error");
 	}
