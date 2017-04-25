d258cbf6387f438ad443285fdc72db9adabf2acf
Jay <24zhangjie@gmail.com>
2017-04-25 13:41:56 +0800

    open file for given filepath

--- 3_fileio/open_file.c
+++ 3_fileio/open_file.c
@@ -6,20 +6,17 @@
  * read a specified file , and print to stdout
  *
  */
-int main(void)
+int main(int argc, char *arg[])
 {
 	char buf[BUFFSIZE];
 	int n;
-	int fd = open("test.txt",O_RDWR,FILE_MODE);
-	//int fd = open(file,O_RDWR,FILE_MODE);
+	//int fd = open("test.txt",O_RDWR,FILE_MODE);
+	int fd = open(arg[1],O_RDWR,FILE_MODE);
 	if(fd<0)
 		err_sys("open filed error!");
 
-//	printf("fd=%d\n",fd);
 	while((n=read(fd,buf,BUFFSIZE))>0)
 	{
-//		printf("n=%d\n",n);
-//		printf("buf = %s\n",buf);
 		if(write(STDOUT_FILENO,buf,n)!=n)
 			err_sys("write error");
 	}
